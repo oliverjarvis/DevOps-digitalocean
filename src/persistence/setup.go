a88626ad1ff992246c7e8bb846779f0a6db11be3
Oliver Jarvis <oliversimonjarvis@gmail.com>
2023-03-01 08:59:28 +0100

    hotfix2

--- src/persistence/setup.go
+++ src/persistence/setup.go
@@ -10,24 +10,22 @@ import (
 var db *gorm.DB
 
 func GetDbConnection() *gorm.DB {
-	dsn := "host=minitwit_db user=postgres password=postgres dbname=postgres port=5432 sslmode=disable"
+	
+	return db
+}
 
-	if db == nil{
-		db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
-		
-		if err != nil {
-			panic("Failed to connect to database")
-		}
-		if db != nil{
-			return db
-		}
+func InitDbConnection() *gorm.DB {
+	dsn := "host=minitwit_db user=postgres password=postgres dbname=postgres port=5432 sslmode=disable"
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic("Failed to connect to database")
 	}
 
 	return db
 }
 
 func ConfigurePersistence() {
-	db := GetDbConnection()
+	db := InitDbConnection()
 
 	applyMigrations(db)
 	seed(db)
